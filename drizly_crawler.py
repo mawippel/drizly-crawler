aecd31c71d4a3fe2b662da775c970d5bbd58a58a
Marcelo Wippel <mawippel2@hotmail.com>
2020-06-24 15:50:45 -0300

    feat: crawling beers and characteristics

--- drizly_crawler.py
+++ drizly_crawler.py
@@ -1,6 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+BASE_URL = 'https://drizly.com'
+dr = None
+
 
 def crawl():
-    passa
+    dr = webdriver.Chrome(ChromeDriverManager().install())
+    dr.get(BASE_URL+'/beer/ale/ipa/c15')
+    soup = BeautifulSoup(dr.page_source, "html.parser")
+
+    beers = soup.findAll(
+        'li', {"class": 'CatalogGrid__CatalogListItem___3S6W5'})
+    for beer in beers:
+        anchor = beer.find('a')
+        print(BASE_URL + anchor['href'])
+        get_beer_characteristics(dr, BASE_URL + anchor['href'])
+
+
+def get_beer_characteristics(dr, href):
+    dr.get(href)
+    soup = BeautifulSoup(dr.page_source, "html.parser")
+
+    characteristics = soup.findAll(
+        'dl', {"class": 'PDPAttributesAndReviews__row___3T_tv'})
+    for char in characteristics:
+        dt = char.find('dt')
+        dd = char.find('dd')
+        print(dt.text + ' : ' + dd.text)
 
 
 if __name__ == "__main__":
