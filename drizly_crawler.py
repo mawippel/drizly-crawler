a1f61eacb0c3ba3ba08c762d8a0235f441d5be99
Marcelo Wippel <mawippel2@hotmail.com>
2020-06-26 17:53:35 -0300

    finishing the crawler

--- drizly_crawler.py
+++ drizly_crawler.py
@@ -3,14 +3,24 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-BASE_URL = 'https://drizly.com'
+BASE_URL = 'https://drizly.com/beer/ale/ipa/c15'
 dr = None
 
 
 def crawl():
     dr = webdriver.Chrome(ChromeDriverManager().install())
 
-    dr.get(BASE_URL+'/beer/ale/ipa/c15')
+    page = 1
+    while True:
+        try:
+            fetch_beers(dr, '/page{}'.format(page))
+            page += 1
+        except:
+            break
+
+
+def fetch_beers(dr, page_sufix):
+    dr.get(BASE_URL + page_sufix)
     soup = BeautifulSoup(dr.page_source, "html.parser")
 
     beers = soup.findAll(
@@ -19,7 +29,6 @@ def crawl():
         anchor = beer.find('a')
         print(BASE_URL + anchor['href'])
         get_beer_characteristics(dr, BASE_URL + anchor['href'])
-
 
 def get_beer_characteristics(dr, href):
     dr.get(href)
